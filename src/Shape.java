289b0c0e47d94bb4d8a5fe7343d7caed1d392179
Amy <Amy@10.32.51.125>
2017-08-29 16:03:31 -0400

    Added return statements

--- src/Shape.java
+++ src/Shape.java
@@ -4,11 +4,11 @@ public class Shape<T>{
 	public int sides;
 	
 	public double Area(T shape){
-		
+		return 0;
 	}
 	
 	public double Perimeter(T shape){
-		
+		return 0;
 	}
 	
 	public void scale(T shape){
