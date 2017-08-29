8b0b60e9e934a6b64b5839df8775f4a62196059d
Amy <Amy@10.32.51.125>
2017-08-29 15:57:53 -0400

    Added generic methods without implementation

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,17 @@
 
-public class Shape {
-
+public class Shape<T>{
+	
+	public int sides;
+	
+	public double Area(T shape){
+		
+	}
+	
+	public double Perimeter(T shape){
+		
+	}
+	
+	public void scale(T shape){
+		
+	}
 }
