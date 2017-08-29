2b63862992b1e7d26c709808a74b3714bd3b1546
Amy <Amy@10.32.51.125>
2017-08-29 18:52:23 -0400

    Added Rectangle implementation

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,12 @@
 
 public class Rectangle {
 
+	int numSides = 0;
+	double height;
+	double width;
+	
+	public Rectangle(double height, double width){
+		this.height = height;
+		this.width = width;
+	}
 }
