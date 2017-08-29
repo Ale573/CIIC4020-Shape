b99d69cbcbf795861653b1bdae60aaccbab9c298
Amy <Amy@10.32.51.125>
2017-08-29 18:43:16 -0400

    Fized Triangle constructor with exception

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,16 +1,21 @@
 
 public class Triangle {
 
-	private int sides;
+	private int sides= 3;
 	private double a;
 	private double b;
 	private double c;
 	
-	public Triangle(int sides, double a, double b, double c){
-		this.sides = sides;
-		this.a = a;
-		this.b = b;
-		this.c = c;
+	public Triangle(double a, double b, double c) throws Exception{
+	
+		if(isATriangle(a, b, c)){
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+		else {
+			throw new Exception(" The data that you provide does not form a triangle.");
+		}
 		
 	}
 	
@@ -18,19 +23,14 @@ public class Triangle {
 		return b;
 	}
 	
-	public double getHeight() throws Exception{
+	public double getHeight(){ 
 		
-		if(isATriangle()){
 			double angle = Math.atan(c/a);
 			double height = angle * a;
 			return height;
-		}
-		else {
-			throw new Exception(" The data that you provide does not form a triangle.");
-		}
 	}
 	
-	public boolean isATriangle(){
+	public boolean isATriangle(double a, double b, double c){
 		
 		if((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (b * b) + (c * c) == (a * a)){
 			return true;			
