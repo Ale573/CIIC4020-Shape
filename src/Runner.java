76fb5c880f3cd89eb612b848ef5a3d9c480ca676
Ale <amatos573@gmial.com>
2017-08-29 17:17:23 -0400

    Added methods to the Triangle class.

--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,8 @@ public class Runner {
 
 	public static void main(String[] args){
 		
+		Shape<Triangle> triangle = new Shape<Triangle>();
+		
+		
 	}
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,4 +1,41 @@
 
 public class Triangle {
 
+	private int sides;
+	private double a;
+	private double b;
+	private double c;
+	
+	public Triangle(int sides, double a, double b, double c){
+		this.sides = sides;
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		
+	}
+	
+	public double getBase(){
+		return b;
+	}
+	
+	public double getHeight() throws Exception{
+		
+		if(isATriangle()){
+			double angle = Math.atan(c/a);
+			double height = angle * a;
+			return height;
+		}
+		else {
+			throw new Exception(" The data that you provide does not form a triangle.");
+		}
+	}
+	
+	public boolean isATriangle(){
+		
+		if((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (b * b) + (c * c) == (a * a)){
+			return true;			
+		}
+		return false;
+		
+	}
 }
