e998d6f4779ccca30615ff4ab14642f003b47f21
Ale <amatos573@gmial.com>
2017-09-01 18:25:32 -0400

    Trying to fix height

--- src/Runner.java
+++ src/Runner.java
@@ -2,10 +2,14 @@
 public class Runner {
 
 	public static void main(String[] args) throws Exception{
-		Triangle figure = new Triangle(7, 10, 15);
-		Shape<Triangle> triangle = new Shape(figure);
-		System.out.println(triangle.getArea(3, figure.getBase(), figure.getHeight()));
-		System.out.println(triangle.getPerimeter(7, 10, 15, 0));
+		Triangle figure = new Triangle(3,4,5);
+		Shape<Triangle> triangle = new Shape<Triangle>(figure);
+		
+		double area = triangle.getArea(3, figure.getBase(), figure.getHeight());
+		
+		System.out.printf("%.2f%n", figure.getHeight());
+		System.out.println(area);
+		
 		
 	}
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,13 +1,13 @@
 
 public class Triangle {
 
-	private int sides= 3;
+	private int sides = 3;
 	private double a;
 	private double b;
 	private double c;
-	
+
 	public Triangle(double a, double b, double c) throws Exception{
-	
+
 		if(isATriangle(a, b, c)){
 			this.a = a;
 			this.b = b;
@@ -16,26 +16,27 @@ public class Triangle {
 		else {
 			throw new Exception(" The data that you provide does not form a triangle.");
 		}
-		
+
 	}
-	
+
 	public double getBase(){
 		return b;
 	}
-	
+
 	public double getHeight(){ 
-		
-			int angle = (int)Math.atan(c/a);
-			double height = angle * a;
-			return height;
+
+		double number = ((a*a) + (b*b) - (c*c))/(2*a*b);
+		int angle = (int) Math.acos(number);
+		double height = Math.sin(angle) * b;
+		return height;
 	}
-	
+
 	public boolean isATriangle(double a, double b, double c){
-		
+
 		if((a + b > c ) && (a + c > b) && (b + c > a)){
 			return true;			
 		}
 		return false;
-		
+
 	}
 }
